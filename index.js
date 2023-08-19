fb42ded739f85da570238c1331f249896cf50f83
pushkar2022 <103018777+pushkar2022@users.noreply.github.com>
2023-08-19 18:11:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+const port=process.env.PORT || 501
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect("mongodb+srv://pushkar:12345@cluster0.vunzghc.mongodb.net/chat?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -25,8 +26,8 @@ mongoose
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-const server = app.listen(process.env.PORT, () =>
-  console.log(`Server started on ${process.env.PORT}`)
+const server = app.listen(port, () =>
+  console.log(`Server started on ${port}`)
 );
 const io = socket(server, {
   cors: {
